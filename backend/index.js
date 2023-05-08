9620a37b00d655c226adc00ef5ab0770923597ca
atabahar <abahar350@insite.4cd.edu>
2023-05-08 15:43:09 -0700

    dashboard with other pages not connected yet

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 const  PORT = 3003;
+
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -14,11 +15,12 @@ const db = mysql.createConnection({
     database:"bank" 
 })
 
-//localhost 3003 reads our message 
+//localhost 3003 reads our message from this page backend server
 app.get("/", (req,res)=>{
   res.json("hello, this is the backend")  
 })
-//localhost 3003/users shows users from database 
+
+//Another request - localhost 3003/users shows users from database 
 app.get("/users", (req,res)=>{
     const q = "SELECT * FROM users"
     db.query(q, (err,data)=>{
@@ -26,9 +28,9 @@ app.get("/users", (req,res)=>{
         return res.json(data)
     })
   })
-//api
 
-app.post("/signUp", (req,res) => {
+//api -sending user desc.
+app.post("/users", (req,res) => {
   const q = "INSERT INTO users (`username`, `password`, `pin`) VALUES  (?)";
   const values = [
     req.body.username,
@@ -40,22 +42,23 @@ app.post("/signUp", (req,res) => {
     if (err) return res.json(err);
     return res.json("");
   });
-});
+}); 
+/*
 app.post("/login", (req,res) => {
-  const q = "SELECT * FROM users WHERE `username` = ? AND `password` = ?";
+  const q = "SELECT * FROM users WHERE `id` = ? AND `username` = ? AND `password` = ?";
   db.query(q, [req.body.username, req.body.password], (err, data) => {
     if (err) { 
       return res.json("Error"); 
     }
     if (data.length > 0) {
-      return res.json("succes");
+      return res.json("success");
     }else {
       return res.json("Fail");
 
     }
   });
 });
-
+*/
 app.listen(PORT, ()=>{
     console.log("connected to backend!")
 })
